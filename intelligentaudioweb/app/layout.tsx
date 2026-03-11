356148fefcc33c74682d563b82db4d6271184f0a
GREENCOM <binarybeat@icloud.com>
2026-03-11 14:38:33 +0100

    Reinstalled analytics

--- intelligentaudioweb/app/layout.tsx
+++ intelligentaudioweb/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 /*import Header from "@/components/header";*/
 import Footer from "@/components/footer";
 import Navbar from "@/components/Navbar";
@@ -82,6 +82,7 @@ export default function RootLayout({
                         </Badge>
                     </div>
                     {children}
+                    <Analytics/>
                 </main>
                 <Footer />
                 <NeuralBottomBar /> 
